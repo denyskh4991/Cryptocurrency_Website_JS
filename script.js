610d1c694c979e089e43e61e59383aaf44c6de4a
denyskh4991 <denyskh4991@gmail.com>
2024-11-10 19:53:54 +0100

    Adding actual prices to the website

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let btc = document.getElementById("bitcoin");
+let eth = document.getElementById("ethereum");
+let doge = document.getElementById("dogecoin");
+
 let settings = {
     "async": true,
     "scrossDomain": true,
@@ -6,5 +10,7 @@ let settings = {
     "headers": {}
 }
 $.ajax(settings).done(function(response) {
-    console.log(response);
+    btc.innerHTML = response.bitcoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    doge.innerHTML = response.dogecoin.usd;
 });
